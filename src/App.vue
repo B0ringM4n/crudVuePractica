31832cf1a2dc197cd6cf731c58bdfc0a0672a9be
B0ringM4n <72772180+B0ringM4n@users.noreply.github.com>
2021-01-07 11:50:07 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <NavBar />
+      <router-view/>
+    <Footer />
+  </div>
+</template>
+
+<script>
+import NavBar from './components/NavBar.vue'
+import Footer from './components/Footer.vue'
+
+export default {
+  name: 'app',
+  components: {
+    NavBar,
+    Footer
+  }
+}
+</script>
+
+<style>
+.navbar-light .navbar-nav .nav-link  {
+  color: white!important;
+}
+</style>
+
+
